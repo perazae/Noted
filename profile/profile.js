1774813a1496a0d5fad6b99cae4e58252b8aa0af
Juan Espinoza <jesp9040@gmail.com>
2023-12-14 13:31:15 -0800

    updated profile

--- profile/profile.js
+++ profile/profile.js
@@ -1,28 +1,29 @@
 'use strict';
 
+const loginData = getLoginData();
+
+const loginToken = loginData.token;
+
+const usernameEndpoint = loginData.username;
+
+const stringValue = JSON.stringify(usernameEndpoint)
 
 
-let token = getLoginData();
-    
 
+//   GET /auth/logout
+  const options = {
+    method: "GET",
+    headers: {
+      // This header is how we authenticate our user with the
+      // server for any API requests which require the user
+      // to be logged-in in order to have access.
+      // In the API docs, these endpoints display a lock icon.
+      Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InRlc3QxMjMiLCJpYXQiOjE3MDI1MTA3NjksImV4cCI6MTcwMjU5NzE2OX0.AYsMSYEadpMT1hQCAlydG2w-EsAjJZTJiHHD75K_XzQ`,
+    },
+  };
 
-// Retrieve data from local storage
-var retrievedValue = localStorage.getItem('login-data');
-retrievedValue = JSON.parse(retrievedValue)
-
-let userNameEndpoint = retrievedValue.username;
-
-var myHeaders = new Headers();
-myHeaders.append("Authorization", "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InRlc3QxMjMiLCJpYXQiOjE3MDI1MTA3NjksImV4cCI6MTcwMjU5NzE2OX0.AYsMSYEadpMT1hQCAlydG2w-EsAjJZTJiHHD75K_XzQ");
-
-var requestOptions = {
-  method: 'GET',
-  headers: myHeaders,
-  redirect: 'follow'
-};
-
-fetch(`https://microbloglite.onrender.com/api/users/${userNameEndpoint}`)
-  .then(response => response.text())
+fetch(`https://microbloglite.onrender.com/api/users/${usernameEndpoint}`)
+  .then(response => response.json())
   .then(result => console.log(result))
   .catch(error => console.log('error', error));
 
