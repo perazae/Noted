035fbe47dbded1bebf46fd5965fe2e932af99f05
Juan Espinoza <jesp9040@gmail.com>
2023-12-18 14:17:06 -0800

    Profile updated

--- profile/profile.js
+++ profile/profile.js
@@ -3,28 +3,18 @@
 window.onload = init;
 
 let userData;
+
 async function init() {
-  userData = await getUser();
+  userData = getLoggedInUser();
 }
 
 // returns an object of a user
 // with properties: username, bio, fullName, createdAt, updatedAt
-async function getUser() {
+async function getLoggedInUser() {
+
   const loginData = getLoginData();
 
-  // GET /api/users/:username
-  const options = {
-    method: "GET",
-    headers: {
-      // This header is how we authenticate our user with the
-      // server for any API requests which require the user
-      // to be logged-in in order to have access.
-      // In the API docs, these endpoints display a lock icon.
-      Authorization: `Bearer ${loginData.token}`,
-    },
-  };
-
-  fetch(apiBaseURL + `/api/users/${loginData.username}`, options)
-    .then((response) => response.json())
-    .then((data) => console.log(data))
+    document.getElementById('viewUser').innerHTML = `${loginData.username}</h1>`
 }
+
+
