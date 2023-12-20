d4602694e6de0c5a2e31ebab7c24306fa05f045e
deli123 <liderek63@gmail.com>
2023-12-20 15:42:57 -0800

    create unique post function for profile

---
+++ scripts/create_post_profile.js
@@ -0,0 +1,65 @@
+"use strict";
+const btnCreatePost = document.getElementById("btnCreatePost");
+btnCreatePost.addEventListener("click", clearForm);
+
+const formCreatePost = document.getElementById("formCreatePost");
+formCreatePost.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  // While testing, putting preventDefault() in an if/else statement
+  // prevented the createPost() function from sending a successful POST request
+  // Thus, preventDefault() will be used outside of the following if statement,
+  // and a workaround to closing the modal upon successful submit is created in the
+  // resetCreatePostModal() function
+  if (formCreatePost.checkValidity()) {
+    createPost();
+  }
+
+  formCreatePost.classList.add("was-validated");
+});
+
+function createPost() {
+  const text = document.getElementById("inputPostText").value;
+
+  const options = {
+    method: "POST",
+    body: JSON.stringify({ text }),
+    headers: {
+      Authorization: `Bearer ${getLoginData().token}`,
+      "Content-Type": "application/json",
+    },
+  };
+
+  fetch(apiBaseURL + "/api/posts", options)
+    .then((response) => response.json())
+    .then((newPost) => {
+      resetCreatePostModal();
+      // update posts section
+      updatePostsSection(newPost);
+    })
+    .catch((error) => alert("Ran into server error when creating post"));
+}
+
+// Reset form input fields and close modal
+function resetCreatePostModal() {
+  clearForm();
+
+  // Manually close the modal
+  const modalCreatePost = document.getElementById("modalCreatePost");
+  const modal = bootstrap.Modal.getInstance(modalCreatePost);
+  modal.hide();
+}
+
+// Remove present validation on the form and reset text field
+function clearForm() {
+  const formCreatePost = document.getElementById("formCreatePost");
+  formCreatePost.classList.remove("was-validated");
+  document.getElementById("inputPostText").value = "";
+}
+
+// update the post section on profile page after creating a new post
+// only if the current profile page belongs to the current user
+function updatePostsSection(newPost) {
+  // DOM manipulation
+  
+}
