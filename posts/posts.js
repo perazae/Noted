7a53cb23d4f2aa2d773d1a1c2eba00863c5128c4
perazae <edsonperaza160@gmail.com>
2023-12-22 14:39:29 -0800

    Added Scroll Top Button when user scrolls past 10px

--- posts/posts.js
+++ posts/posts.js
@@ -15,6 +15,7 @@ let loading = false;
 //checks to see if there are more posts
 let hasMorePosts = true;
 let initialOffset = 10;
+
 /**This function will allow the user to load posts as they scroll
  * in other words, it is an infinite scrolling function until the last post is displayed
  */
@@ -41,6 +42,32 @@ window.onscroll = async function (event) {
     }
   }
 };
+//Get the button
+let mybutton = document.getElementById("btn-back-to-top");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (
+    document.body.scrollTop > 20 ||
+    document.documentElement.scrollTop > 20
+  ) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+// When the user clicks on the button, scroll to the top of the document
+mybutton.addEventListener("click", backToTop);
+
+function backToTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
 async function displayAllUserPosts(initialOffset) {
   const loginData = getLoginData();
   const token = loginData.token;
