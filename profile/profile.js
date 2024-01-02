402edf44c81a981ea581e101fee6bad854a038b1
Juan Espinoza <jesp9040@gmail.com>
2024-01-02 15:14:33 -0800

    Profile Update
    Background image and cover changes on another profile

--- profile/profile.js
+++ profile/profile.js
@@ -52,6 +52,43 @@ async function init() {
   if (username !== currentUser) {
     btnEditProfile.remove();
     document.getElementById("modalEditProfile").remove();
+
+  // colors array for user profiles
+    const colors = ["#3498db", "#e74c3c", "#2ecc71", "#f39c12", "#9b59b6", "#1abc9c", "#d35400", "#34495e", "#c0392b", "#27ae60"]
+    ;
+    const covers = [
+      `url(https://wallpapercave.com/wp/iu98stB.jpg)`,
+      `url(https://wallpaperaccess.com/full/1713744.jpg)`,
+      `url(https://wallpaperaccess.com/full/2825826.gif)`,
+      `url(https://wallpapers.com/images/hd/orange-trees-in-fall-scenic-kef79jh8aaok0jty.jpg)`,
+      `url(https://wallpaperaccess.com/full/316663.jpg)`,
+      `url(https://cdna.artstation.com/p/marketplace/presentation_assets/002/977/574/large/file.jpg?1693378178)`,
+      `url(https://i.pinimg.com/originals/67/7a/33/677a33611a9910bfdc7503de78c08323.jpg)`,
+      `url(https://wallpapers.com/images/featured/4k-oaax18kaapkokaro.jpg)`,
+      `url(https://wallpaperaccess.com/full/7270387.gif)`,
+      `url(https://wallpaperaccess.com/full/8088665.gif)`
+    ];
+
+// Function to shuffle the color array using Fisher-Yates algorithm
+// function shuffleArray(array) {
+//     for (let i = array.length - 1; i > 0; i--) {
+//         const j = Math.floor(Math.random() * (i + 1));
+//         [array[i], array[j]] = [array[j], array[i]];
+//     }
+// }
+
+// shuffleArray(colors);
+// shuffleArray(covers);
+
+let randomCover = Math.floor(Math.random() * covers.length)
+let randomColor = Math.floor(Math.random() * colors.length)
+document.getElementById('backgroundColor').style.background = covers[randomCover];
+document.getElementById('backgroundColor').style.backgroundSize = "cover";
+document.getElementById('backgroundColor').style.backgroundRepeat = "no-repeat";
+document.getElementById('backgroundColor').style.backgroundPosition = "center center";
+document.getElementById('backgroundColor').style.backgroundAttachment = "fixed";
+document.getElementById('coverColor').style.background = colors[randomColor];
+
   } else {
     btnEditProfile.classList.remove("d-none");
   }
@@ -59,8 +96,10 @@ async function init() {
   // change the user's @ tag and show their posts
   changeUserTag(username);
   viewProfilePosts(username);
-  displayFriends();
-}
+  displayFriends(); 
+}  
+console.log(document.getElementById('profileFriends')) 
+console.log("plop")
 
 // change the user's @ tag
 function changeUserTag(username) {
@@ -181,6 +220,7 @@ function displayFriends() {
 
   const loginData = getLoginData();
   const userToken = loginData.token;
+  const currentUser = loginData.username;
 
   myHeaders.append("Authorization", `Bearer ${userToken}`);
 
@@ -190,27 +230,24 @@ function displayFriends() {
     redirect: "follow",
   };
 
-  let friendList = "";
+  let friendList = [];
 
   fetch(`${apiBaseURL}/api/users?limit=10&offset=5`, requestOptions)
     .then((response) => response.json())
     .then((friends) => {
-      console.log(friends);
       for (let index = 0; index < 10; index++) {
         const friendUsername = friends[index].username;
 
-        // Check if the friendList already contains the current username
-        if (!friendList.includes(friendUsername)) {
+        // Check if the friendUsername is not the current user
           let friend = friendUsername;
           friendList += `
-          <div class="list-group ">
-            <a href="/profile/?username=${friend}" class="list-group-item list-group-item-action">@${friend}</a>
-          </div>
-        `;
-        }
+            <div class="list-group ">
+              <a href="/profile/?username=${friend}" class="list-group-item list-group-item-action">@${friend}</a>
+            </div>
+          `;   
       }
-
       document.getElementById("profileFriends").innerHTML = friendList;
     })
     .catch((error) => showToast(false, "Error retrieving friend list."));
 }
+
